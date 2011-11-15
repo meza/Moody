a4bdfec4b5c52ae2c2cdb37a5dbbb804b5123d6c
meza <meza@meza.hu>
2011-11-15 17:08:30 +0100

    MVN integration

---
+++ MoodyTest/src/main/java/hu/meza/android/Moody/test/LocationFaker.java
@@ -0,0 +1,63 @@
+package hu.meza.android.Moody.test;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.util.Log;
+
+public class LocationFaker {
+
+	private LocationManager lm;
+
+	private static final boolean REQUIRES_NETWORK = false;
+	private static final boolean REQUIRES_SATELLITE = false;
+	private static final boolean REQUIRES_CELL = false;
+	private static final boolean HAS_MONETARY_COST = false;
+	private static final boolean SUPPORTS_ALTITUDE = true;
+	private static final boolean SUPPORTS_SPEED = true;
+	private static final boolean SUPPORTS_BEARING = true;
+	private static String TAG = LocationFaker.class.getSimpleName();
+
+	public LocationFaker(LocationManager locationManager) {
+		lm = locationManager;
+	}
+
+	public Object getFakedManager() {
+		return lm;
+	}
+
+	public void createTestProvider(String testProvider, long time) {
+		if (!hasTestProvider(testProvider)) {
+			lm.addTestProvider(testProvider, REQUIRES_NETWORK,
+					REQUIRES_SATELLITE, REQUIRES_CELL, HAS_MONETARY_COST,
+					SUPPORTS_ALTITUDE, SUPPORTS_SPEED, SUPPORTS_BEARING,
+					android.location.Criteria.POWER_LOW,
+					android.location.Criteria.ACCURACY_COARSE);
+			lm.setTestProviderEnabled(testProvider, true);
+			lm.setTestProviderStatus(testProvider, LocationProvider.AVAILABLE,
+					null, time);
+		}
+		lm.setTestProviderStatus(testProvider, LocationProvider.AVAILABLE,
+				null, time);
+	}
+
+	private boolean hasTestProvider(String testProvider) {
+		return lm.getAllProviders().contains(testProvider);
+	}
+
+	public void publishMockLocation(String testProvider, double latitude,
+			double longitude, long time) {
+		lm.setTestProviderEnabled(testProvider, true);
+		Location newLocation = new Location(testProvider);
+
+		newLocation.setLatitude(latitude);
+		newLocation.setLongitude(longitude);
+		newLocation.setTime(time);
+		newLocation.setAccuracy(0);
+		lm.setTestProviderLocation(testProvider, newLocation);
+		
+		Log.i(TAG, "I issued an update to " + testProvider + " "
+				+ Integer.toString(lm.hashCode()));
+
+	}
+}
---
+++ MoodyTest/src/main/java/hu/meza/android/Moody/test/MoodyTest.java
@@ -0,0 +1,133 @@
+package hu.meza.android.Moody.test;
+
+import hu.meza.android.Moody.MoodyActivity;
+import hu.meza.android.Moody.R;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.test.ActivityUnitTestCase;
+import android.test.IsolatedContext;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MoodyTest extends ActivityUnitTestCase<MoodyActivity> {
+
+	public static final String TAG = "MoodyTest";
+	private Intent mStartIntent;
+	private Context mcontext;
+	private LocationFaker locationFaker;
+	private EditText field;
+
+	public MoodyTest() {
+		super(MoodyActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		locationFaker = new LocationFaker(
+				(LocationManager) getInstrumentation().getTargetContext()
+						.getSystemService(Context.LOCATION_SERVICE));
+		locationFaker.createTestProvider(MoodyActivity.TEST_PROVIDER,
+				System.currentTimeMillis());
+
+		mcontext = new IsolatedContext(null, getInstrumentation().getContext()) {
+			@Override
+			public Object getSystemService(final String pName) {
+				if (pName.equals(Context.LOCATION_SERVICE)) {
+					return locationFaker.getFakedManager();
+				}
+				return getInstrumentation().getTargetContext()
+						.getSystemService(pName);
+			}
+		};
+		mStartIntent = new Intent(mcontext, MoodyActivity.class);
+	}
+	
+	@SmallTest
+	public void testThatTheTestProviderIsInUse() {
+		startActivity(mStartIntent, null, null);
+		setActivityContext(mcontext);
+		getActivity().onCreate(null);
+		assertEquals("Test provider not injected", MoodyActivity.TEST_PROVIDER,
+				getActivity().getBestProvider());
+		getActivity().onDestroy();
+	}
+
+	@MediumTest
+	public void testIfMyLocationIsProperlyResolved() {
+		testForLocation(37.7827, -122.421);
+	}
+
+	@MediumTest
+	public void testIfMyLocationIsProperlyResolved2() {
+		testForLocation(47.49897, 19.0407);
+	}
+
+	private void testForLocation(double latitude, double longitude) {
+		startActivity(mStartIntent, null, null);
+		getActivity().onCreate(null);
+		field = (EditText) getActivity()
+				.findViewById(R.id.locationInput);
+		setMyLatLong(latitude, longitude);
+		String actual = field.getText().toString();
+		String expected = String.format("%2.7f, %2.7f", latitude, longitude);
+		assertEquals(expected, actual);
+		getActivity().onDestroy();
+	}
+
+	private void setMyLatLong(double latitude, double longitude) {
+
+		class MyListener implements LocationListener {
+			private Location receivedLocation = null;
+
+			public Location receivedLocation() {
+				return receivedLocation;
+			}
+
+			@Override
+			public void onLocationChanged(Location location) {
+				receivedLocation = location;
+				Log.d(TAG, String.format("onLocationChanged -> [lat:%f|long:%f]", location.getLatitude(),location.getLongitude()));
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+				Log.d(TAG, String.format("onProviderDisabled: %s", provider));
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				Log.d(TAG, String.format("onProviderEnabled: %s", provider));
+
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+				// TODO Auto-generated method stub
+
+			}
+		}
+		MyListener ll = new MyListener();
+
+		((LocationManager) locationFaker.getFakedManager())
+				.requestSingleUpdate(MoodyActivity.TEST_PROVIDER, ll, getActivity().getMainLooper());
+	
+		locationFaker.publishMockLocation(MoodyActivity.TEST_PROVIDER,
+				latitude, longitude, System.currentTimeMillis());
+		try {
+			Thread.sleep(1000);
+			assertNotNull("Location not received", ll.receivedLocation());
+		} catch (InterruptedException e) {
+			fail("Waiting for location has been interrupted");
+		}
+	}
+
+}
