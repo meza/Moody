d062d127bfeafd64b2ab8add145527fe753e037b
meza <meza@meza.hu>
2011-11-17 11:38:42 +0100

    Dummy test

---
+++ MoodyApp/src/test/java/hu/meza/android/MoodyApp/test/dummyTest.java
@@ -0,0 +1,14 @@
+package hu.meza.android.MoodyApp.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class dummyTest extends TestCase {
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+	}
+
+}
