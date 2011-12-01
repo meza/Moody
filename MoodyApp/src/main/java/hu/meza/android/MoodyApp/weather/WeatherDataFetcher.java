35935f7321942ffa6e9e96b819d6852050d4a7ed
meza <meza@meza.hu>
2011-12-01 11:48:21 +0300

    fetcher

--- MoodyApp/src/main/java/hu/meza/android/MoodyApp/weather/WeatherDataFetcher.java
+++ MoodyApp/src/main/java/hu/meza/android/MoodyApp/weather/WeatherDataFetcher.java
@@ -1,20 +1,43 @@
 package hu.meza.android.MoodyApp.weather;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 
-import android.location.Location;
+public class WeatherDataFetcher
+{
 
-public class WeatherDataFetcher {
+	public static String SERVICE_URL = "http://free.worldweatheronline.com/feed/weather.ashx";
+	public static String API_KEY = "aaabbbccceeedddedededede";
 
 	private HttpClient client = null;
-	
-	public WeatherDataFetcher(HttpClient client) {
+
+
+	public WeatherDataFetcher(HttpClient client)
+	{
 		this.client = client;
 	}
-	
-	public String getPredictionForLocation(Location testLocation) {
-		return "";
-		
+
+
+	public void getPredictionForLocation(int longitude, int latitude)
+		throws ClientProtocolException, IOException
+	{
+		HttpGet request = new HttpGet();
+		try {
+			String finalUri = String.format(
+				"%s?0=%s,%s&format=json&num_of_days=2&key=%s",
+				SERVICE_URL,
+				longitude,
+				latitude,
+				API_KEY);
+			request.setURI(new URI(finalUri));
+		} catch (URISyntaxException e1) {
+			throw new RuntimeException("Syntax error in test");
+		}
+		client.execute(request);
 	}
 
 }
--- MoodyApp/src/test/java/hu/meza/android/MoodyApp/test/weather/WeatherDataFetcherTest.java
+++ MoodyApp/src/test/java/hu/meza/android/MoodyApp/test/weather/WeatherDataFetcherTest.java
@@ -1,38 +1,54 @@
 package hu.meza.android.MoodyApp.test.weather;
 
 import hu.meza.android.MoodyApp.weather.WeatherDataFetcher;
-
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
-import android.location.Location;
+public class WeatherDataFetcherTest
+{
 
-public class WeatherDataFetcherTest {
+	private HttpClient client;
+	private ArgumentCaptor<HttpClient> argument;
+	private WeatherDataFetcher object;
 
-	private static String SERVICE_URL = "http://free.worldweatheronline.com/feed/weather.ashx";
-	private static String API_KEY = "aabc1040d0162127111611";
-	
+
+	@Before
+	public void setUp()
+	{
+		client = Mockito.mock(HttpClient.class);
+		argument = ArgumentCaptor.forClass(HttpClient.class);
+		object = new WeatherDataFetcher(client);
+	}
+
+
 	@Test
-	public void test() throws ClientProtocolException, IOException, URISyntaxException {
-		String url = SERVICE_URL+"?q=47.50,19.04&format=json&num_of_days=2&key="+API_KEY;
-		String expected = "lófaszjésonszting";
-		HttpGet request = Mockito.mock(HttpGet.class);
-        HttpResponse resp = Mockito.mock(HttpResponse.class);
-		HttpClient client = Mockito.mock(HttpClient.class);
-		Mockito.when(client.execute(request)).thenReturn(resp);
-		WeatherDataFetcher object = new WeatherDataFetcher(client);
-		Location testLocation = Mockito.mock(Location.class);
-		String actual = object.getPredictionForLocation(testLocation);
-		String urlX = String.format("{0}?0={1},{2}&format=json&num_of_days=2&key={3}", SERVICE_URL, testLocation.getLongitude(), testLocation.getLatitude(), API_KEY);
-		//Mockito.verify(request, Mockito.times(1)).setURI(new URI(SERVICE_URL));
+	public void test()
+		throws ClientProtocolException, IOException, URISyntaxException
+	{
+		int longitude = 10;
+		int latitude = 21;
+
+		object.getPredictionForLocation(longitude, latitude);
+
+		// Fetch the request object
+		Mockito.verify(client).execute((HttpGet) argument.capture());
+
+		String finalUri = String.format(
+			"%s?0=%s,%s&format=json&num_of_days=2&key=%s",
+			WeatherDataFetcher.SERVICE_URL,
+			longitude,
+			latitude,
+			WeatherDataFetcher.API_KEY);
+		Assert.assertEquals(finalUri, ((HttpGet) argument.getValue()).getURI()
+			.toString());
 	}
 
 }
